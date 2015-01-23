e96961e0fbc2f463dbfeca452a0850002d18112f
Mark Featherston <mark@embeddedarm.com>
2015-01-23 09:28:42 -0700

    Export "rcause" as a variable to be used in scripting.  Users can use this to script, eg fallback to another kernel if watchdog keeps resetting, or passed to "cmdline_append" if they want to parse it in linux. Export "model" so usb or image scripts can tell the difference between 7970/4900, mx28 series or future boards.

--- arch/arm/mach-imx/cpu.c
+++ arch/arm/mach-imx/cpu.c
@@ -48,7 +48,7 @@ u32 get_imx_reset_cause(void)
 }
 
 #if defined(CONFIG_DISPLAY_CPUINFO) && !defined(CONFIG_XPL_BUILD)
-static char *get_reset_cause(void)
+char *get_reset_cause(void)
 {
 	switch (get_imx_reset_cause()) {
 	case 0x00001:
