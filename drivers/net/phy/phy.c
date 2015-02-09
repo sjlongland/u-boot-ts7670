73b32fc94e99c2d16f10075047546ed9cf9e7953
Mark Featherston <mark@embeddedarm.com>
2015-02-09 10:15:06 -0700

    The built in timer 0 doesn't reproduce many different macs when run from startup.  This change assigns the random mac address (only used in production) on link up instead of phy init.

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -219,6 +219,8 @@ int genphy_config_aneg(struct phy_device *phydev)
 	return result;
 }
 
+extern int random_mac;
+
 /**
  * genphy_update_link - update link status in @phydev
  * @phydev: target phy_device struct
@@ -275,6 +277,14 @@ int genphy_update_link(struct phy_device *phydev)
 			mdelay(50);	/* 50 ms */
 		}
 		printf(" done (%dms)\n", i);
+
+		/* The timer used in eth_random_addr doesn't work very well from poweron.  If
+		 * I need a random mac, getting it with linkup seems much more unique */
+		if(random_mac) {
+			uchar enetaddr[6];
+			eth_random_addr(enetaddr);
+			eth_setenv_enetaddr("ethaddr", enetaddr);
+		}
 		phydev->link = 1;
 	} else {
 		/* Read the link a second time to clear the latched state */
