878aeb5d8670a097d20f61538cc5aa17a61323c5
Stuart Longland <me@vk4msl.com>
2026-05-31 13:50:37 +1000

    arm: ts7670d led: Update to support new LED APIs

--- board/technologic/ts7670d/led.c
+++ board/technologic/ts7670d/led.c
@@ -6,10 +6,12 @@
 #include <asm/arch/iomux.h>
 #include <config.h>
 #include <linux/types.h>
+#ifdef CONFIG_LED_STATUS
 #include <status_led.h>
+#endif
 
-static unsigned int saved_state[4] = {STATUS_LED_OFF,
-	STATUS_LED_OFF, STATUS_LED_OFF, STATUS_LED_OFF};
+static unsigned int saved_state[4] = {CONFIG_LED_STATUS_OFF,
+	CONFIG_LED_STATUS_OFF, CONFIG_LED_STATUS_OFF, CONFIG_LED_STATUS_OFF};
 
 iomux_cfg_t const led_pads[] = {
 	CONFIG_RED_LED, // Red
@@ -26,49 +28,49 @@ void coloured_LED_init(void)
 void red_led_on(void)
 {
 	gpio_direction_output(led_pads[0], 0);
-	saved_state[STATUS_LED_RED] = STATUS_LED_ON;
+	saved_state[STATUS_LED_RED] = CONFIG_LED_STATUS_ON;
 }
 
 void red_led_off(void)
 {
 	gpio_direction_output(led_pads[0], 1);
-	saved_state[STATUS_LED_RED] = STATUS_LED_OFF;
+	saved_state[STATUS_LED_RED] = CONFIG_LED_STATUS_OFF;
 }
 
 void green_led_on(void)
 {
 	gpio_direction_output(led_pads[1], 0);
-	saved_state[STATUS_LED_GREEN] = STATUS_LED_ON;
+	saved_state[STATUS_LED_GREEN] = CONFIG_LED_STATUS_ON;
 }
 
 void yellow_led_off(void)
 {
 	gpio_direction_output(led_pads[2], 1);
-	saved_state[STATUS_LED_YELLOW] = STATUS_LED_OFF;
+	saved_state[STATUS_LED_YELLOW] = CONFIG_LED_STATUS_OFF;
 }
 
 void yellow_led_on(void)
 {
 	gpio_direction_output(led_pads[2], 0);
-	saved_state[STATUS_LED_YELLOW] = STATUS_LED_ON;
+	saved_state[STATUS_LED_YELLOW] = CONFIG_LED_STATUS_ON;
 }
 
 void blue_led_off(void)
 {
 	gpio_direction_output(led_pads[3], 0);
-	saved_state[STATUS_LED_BLUE] = STATUS_LED_OFF;
+	saved_state[STATUS_LED_BLUE] = CONFIG_LED_STATUS_OFF;
 }
 
 void blue_led_on(void)
 {
 	gpio_direction_output(led_pads[3], 1);
-	saved_state[STATUS_LED_BLUE] = STATUS_LED_ON;
+	saved_state[STATUS_LED_BLUE] = CONFIG_LED_STATUS_ON;
 }
 
 void green_led_off(void)
 {
 	gpio_direction_output(led_pads[1], 1);
-	saved_state[STATUS_LED_GREEN] = STATUS_LED_OFF;
+	saved_state[STATUS_LED_GREEN] = CONFIG_LED_STATUS_OFF;
 }
 
 void __led_init(led_id_t mask, int state)
@@ -79,22 +81,22 @@ void __led_init(led_id_t mask, int state)
 void __led_toggle(led_id_t mask)
 {
 	if (STATUS_LED_RED == mask) {
-		if (STATUS_LED_ON == saved_state[STATUS_LED_RED])
+		if (CONFIG_LED_STATUS_ON == saved_state[STATUS_LED_RED])
 			red_led_off();
 		else
 			red_led_on();
 	} else if (STATUS_LED_GREEN == mask) {
-		if (STATUS_LED_ON == saved_state[STATUS_LED_GREEN])
+		if (CONFIG_LED_STATUS_ON == saved_state[STATUS_LED_GREEN])
 			green_led_off();
 		else
 			green_led_on();
 	} else if (STATUS_LED_YELLOW == mask) {
-		if (STATUS_LED_ON == saved_state[STATUS_LED_YELLOW])
+		if (CONFIG_LED_STATUS_ON == saved_state[STATUS_LED_YELLOW])
 			yellow_led_off();
 		else
 			yellow_led_on();
 	} else if (STATUS_LED_BLUE == mask) {
-		if (STATUS_LED_ON == saved_state[STATUS_LED_BLUE])
+		if (CONFIG_LED_STATUS_ON == saved_state[STATUS_LED_BLUE])
 			blue_led_off();
 		else
 			blue_led_on();
@@ -105,28 +107,28 @@ void __led_set(led_id_t mask, int state)
 {
 	switch(mask) {
 	  case STATUS_LED_RED:
-		if(STATUS_LED_ON == state) {
+		if(CONFIG_LED_STATUS_ON == state) {
 			red_led_on();
 		} else {
 			red_led_off();
 		}
 		break;
 	  case STATUS_LED_GREEN:
-		if(STATUS_LED_ON == state) {
+		if(CONFIG_LED_STATUS_ON == state) {
 			green_led_on();
 		} else {
 			green_led_off();
 		}
 		break;
 	  case STATUS_LED_YELLOW:
-		if(STATUS_LED_ON == state) {
+		if(CONFIG_LED_STATUS_ON == state) {
 			yellow_led_on();
 		} else {
 			yellow_led_off();
 		}
 		break;
 	  case STATUS_LED_BLUE:
-		if(STATUS_LED_ON == state) {
+		if(CONFIG_LED_STATUS_ON == state) {
 			blue_led_on();
 		} else {
 			blue_led_off();
