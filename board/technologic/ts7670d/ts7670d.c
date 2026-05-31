45ac94f6d85189a63741f967b0b043b47c1efc49
Stuart Longland <me@vk4msl.com>
2026-05-31 10:14:10 +1000

    arm: ts7670d: Replace `common.h` with `config.h` and friends

--- board/technologic/ts7670d/ts7670d.c
+++ board/technologic/ts7670d/ts7670d.c
@@ -11,14 +11,15 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#include <common.h>
 #include <asm/gpio.h>
 #include <asm/io.h>
 #include <asm/arch/imx-regs.h>
 #include <asm/arch/iomux-mx28.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/sys_proto.h>
+#include <config.h>
 #include <linux/mii.h>
+#include <linux/types.h>
 #include <miiphy.h>
 #include <netdev.h>
 #include <errno.h>
