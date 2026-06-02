48aaeb47a2c580cb92a9f20c94147ea96c804cea
Stuart Longland <me@vk4msl.com>
2026-06-02 22:35:35 +1000

    drivers: ts_wdt: Re-work driver for device model
    - Use a per-device data structure to store timeout
    - Pass device to `dm_i2c_write` not its parent
    - Add some debug statements so we can see what's going on (in theory!)
    
    Driver doesn't work yet, and I see no evidence of it being called!

--- drivers/watchdog/ts_wdt.c
+++ drivers/watchdog/ts_wdt.c
@@ -20,9 +20,12 @@
 #include <dm.h>
 #include <i2c.h>
 #include <log.h>
-#include <watchdog.h>
+#include <wdt.h>
+#include <linux/printk.h>
 
-static uint16_t ts_wdt_timeout;
+struct ts_wdt_priv {
+	uint16_t timeout;
+};
 
 #define TS_DEFAULT_TIMEOUT 30
 
@@ -57,7 +60,7 @@ static int ts_wdt_write(struct udevice *dev, u16 cmd)
 	out[0] = (cmd & 0xff00) >> 8;
 	out[1] = cmd & 0xff;
 
-	return dm_i2c_write(dev->parent, 0, out, 2);
+	return dm_i2c_write(dev, 0, out, 2);
 }
 
 static int ts_wdt_stop(struct udevice *dev)
@@ -67,24 +70,29 @@ static int ts_wdt_stop(struct udevice *dev)
 
 static int ts_wdt_reset(struct udevice *dev)
 {
-	return ts_wdt_write(dev, ts_wdt_timeout);
+	const struct ts_wdt_priv* priv = dev_get_uclass_priv(dev);
+	return ts_wdt_write(dev, priv->timeout);
 }
 
 static int ts_wdt_start(struct udevice *dev, u64 timeout, ulong flags)
 {
+	struct ts_wdt_priv* priv = dev_get_uclass_priv(dev);
 	(void)flags;
 
 	/* Units of 0.1 sec */
 	if (timeout < 100) {
 		/* Too small */
-		return -ENOTSUP;
+		timeout = 100;
 	}
 
-	return ts_wdt_reset(dev, ts_wdt_timeout);
+	priv->timeout = timeout / 100;
+	pr_info("Starting watchdog with delay=%u\n", priv->timeout);
+	return ts_wdt_reset(dev);
 }
 
 static int ts_wdt_expire_now(struct udevice *dev, ulong flags)
 {
+	pr_info("EXPIRE now\n");
 	return ts_wdt_write(dev, TS_WDT_CMD_TIMEOUT_0S2);
 }
 
@@ -96,7 +104,7 @@ static const struct wdt_ops ts_wdt_ops = {
 };
 
 static const struct udevice_id ts_wdt_ids[] = {
-	{ .compatible = "technologicsystems,ts-wdt", },
+	{ .compatible = "technologic,ts-wdt", },
 	{}
 };
 
@@ -105,4 +113,5 @@ U_BOOT_DRIVER(ts_wdt) = {
 	.id = UCLASS_WDT,
 	.of_match = ts_wdt_ids,
 	.ops = &ts_wdt_ops,
+	.per_child_auto = sizeof(struct ts_wdt_priv),
 };
