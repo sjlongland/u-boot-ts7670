de97cd375fa01b428981777f23255b507140913b
Stuart Longland <me@vk4msl.com>
2026-06-07 22:22:12 +1000

    [board] ts7670d: Drop `jpsdboot` read function
    1. it doesn't work, even when you correct the GPIO (it should be
       `MX28_PAD_LCD_D01__GPIO_1_1`)
    2. we can't change the environment location, that's hard-coded in the
       binary (from `.config` actually), so it's a moot point

--- board/technologic/ts7670d/ts7670d.c
+++ board/technologic/ts7670d/ts7670d.c
@@ -33,12 +33,10 @@
 #include <i2c.h>
 
 #define TS7670D_V2_EN_SDPWR     MX28_PAD_PWM3__GPIO_3_28
-#define TS7670D_V2_SDBOOT_JP    MX28_PAD_LCD_D12__GPIO_1_12
 #define TS7670D_V2_POWER_FAIL	MX28_PAD_SSP0_DETECT__GPIO_2_9
 
 DECLARE_GLOBAL_DATA_PTR;
 int random_mac = 0;
-static int ts7670d_get_sdboot_jp(void);
 
 void mx28_adjust_mac(int dev_id, unsigned char *mac)
 {
@@ -90,29 +88,17 @@ int dram_init(void)
 	return mxs_dram_init();
 }
 
-static int ts7670d_get_sdboot_jp(void)
-{
-	gpio_direction_input(TS7670D_V2_SDBOOT_JP);
-	return gpio_get_value(TS7670D_V2_SDBOOT_JP);
-}
-
 int misc_init_r(void)
 {
-	int sdboot = 0;
-
 	env_set("model", "7670D");
-
-	sdboot = ts7670d_get_sdboot_jp();
-
-	if(sdboot) env_set("jpsdboot", "off");
-	else env_set("jpsdboot", "on");
 
 	return 0;
 }
 
 void board_boot_order(u32 *spl_boot_list)
 {
-	if (ts7670d_get_sdboot_jp() > 0) {
+	const char env_device[] = {CONFIG_ENV_EXT4_DEVICE_AND_PART};
+	if (env_device[0] == '0') {
 		spl_boot_list[0] = BOOT_DEVICE_MMC1;
 	} else {
 		spl_boot_list[0] = BOOT_DEVICE_MMC2;
